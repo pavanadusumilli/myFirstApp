5bbbcb3cc3a8fa064895b008906550951cf144d0
pavan <adusumilli.474@gmail.com>
2018-01-24 11:23:45 +0530

    My first repository for the app.

--- app/src/main/java/com/example/padusumi/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/padusumi/myfirstapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.padusumi.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private Button Browse;
+    private File selectedFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Browse = (Button)findViewById(R.id.browse);
+        Browse.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        Log.d(TAG, "onClick: event triggered");
     }
 }
